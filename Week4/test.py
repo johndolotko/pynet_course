eb162619145f9c2f1e20017ca1a81a1470b86785
John Dolotko <john.dolotko@ntlworld.com>
2019-05-20 09:26:27 -0700

    Commit last of week4 files

---
+++ Week4/test.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+import textfsm
+
+template_file = "show_ip_bgp.template"
+template = open(template_file)
+
+with open("show_ip_bgp.txt") as f:
+    raw_text_data = f.read()
+
+# Import python debugger to see what is happening
+import ipdb
+ipdb.set_trace()
+
+# The argument 'template' is a file handle and 'raw_text_data' is a string
+re_table = textfsm.TextFSM(template)
+data = re_table.ParseText(raw_text_data)
+
+template.close()
+
+print("\nPrint the header row which could be used for dictionary construction")
+print(re_table.header)
+print("\nOutput Data: ")
+pprint(data)
+print()
+
